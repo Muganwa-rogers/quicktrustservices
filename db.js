839683c788c35e6909047b2318e8c5c23c983240
Muganwa-rogers <umuganwarogers1@gmail.com>
2025-03-24 14:50:21 +0200

    4th commit

--- db.js
+++ db.js
@@ -6,7 +6,9 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: 3306 // Ensure you're connecting to the correct port
 });
+
 
 db.connect((err) => {
     if (err) {
