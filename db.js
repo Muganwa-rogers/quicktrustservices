c4f5bfd7ae6f08de2e0605d685094a4f475651cb
Muganwa-rogers <umuganwarogers1@gmail.com>
2025-03-24 15:14:30 +0200

    4.5th commit

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
-const mysql = require('mysql');
-require('dotenv').config();
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: 3306 // Ensure you're connecting to the correct port
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
-
 db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err.message);
-        process.exit(1); // Exit the process with an error
-    }
-    console.log('Connected to the MySQL database');
+  if (err) {
+    console.error("Database connection failed:", err);
+    process.exit(1); // Exit if connection fails
+  } else {
+    console.log("Database connected successfully!");
+  }
 });
 
 module.exports = db;
--- server.js
+++ server.js
@@ -43,14 +43,15 @@ app.use("/api/apartments", apartments);
 app.use('/admin',admin)
             
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log('Connecting with:', {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        // port: process.env.DB_PORT,
+        port: process.env.DB_PORT,
     });
     
 });
